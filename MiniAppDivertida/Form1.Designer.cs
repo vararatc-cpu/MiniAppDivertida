92ccaba0a24d87bcd92a6dfbe9149433360c4410
vararatc-cpu <vararatc@unemi.edu.ec>
2025-10-02 16:32:32 -0500

    Primer commit: interfaz con botón y label funcionando

--- MiniAppDivertida/Form1.Designer.cs
+++ MiniAppDivertida/Form1.Designer.cs
@@ -28,18 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(357, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 51);
+            button1.TabIndex = 0;
+            button1.Text = "Dame Click";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(88, 371);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(741, 429);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
     }
 }
--- MiniAppDivertida/Form1.cs
+++ MiniAppDivertida/Form1.cs
@@ -1,10 +1,33 @@
+using System;
+using System.Windows.Forms;
+
 namespace MiniAppDivertida
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random(); // Para generar números aleatorios
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mover el botón a una posición aleatoria dentro del formulario
+            int maxX = this.ClientSize.Width - button1.Width;
+            int maxY = this.ClientSize.Height - button1.Height;
+
+            button1.Left = rnd.Next(0, maxX);
+            button1.Top = rnd.Next(0, maxY);
+
+            // Mensajes aleatorios
+            string[] mensajes = { "¡Hola!", "¡Clic rápido!", "¡Sigue intentando!", "¡Eres rápido!" };
+            label1.Text = mensajes[rnd.Next(mensajes.Length)];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
